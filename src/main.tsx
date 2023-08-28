d81f42d6274aa219fbbf51f5e963e907c73c99ba
Frosty <nayanbastola777@gmail.com>
2023-08-28 14:53:01 -0400

    Route-Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
 import "./global.css";
+import Login from "./pages/welcome/Login";
+import Register from "./pages/welcome/Register";
+import Home from "./pages/home/Home";
+import Product from "./pages/products/Product";
+import Order from "./pages/orders/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  { path: "/register", element: <Register /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+      {
+        path: "/orders",
+        element: <Order />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
