bd9f626b7c3de9baaf70109d5f00c5fb32bd01e7
Frosty <nayanbastola777@gmail.com>
2023-08-28 14:37:48 -0400

    Init Setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <p className="text-2xl">This is a paragraph </p>
+      <h1 className="text-2xl">Doresu Ecommerce</h1>
+      <h2 className="text-2xl">Doresu Ecommerce</h2>
+      <h3 className="text-2xl">Doresu Ecommerce</h3>
+      <h4 className="text-2xl">Doresu Ecommerce</h4>
+      <h5 className="text-2xl">Doresu Ecommerce</h5>
+      <h6 className="text-2xl">Doresu Ecommerce</h6>
+      <span className="text-2xl">This is a span</span>
+    </>
+  );
+}
+
+export default App;
